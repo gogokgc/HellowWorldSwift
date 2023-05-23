2e15a2c5f789a0a9361091df836cbb80185bd8a1
gogokgc <gogokgc@gmail.com>
2023-05-23 15:32:27 +0900

    Initial Commit

---
+++ HellowWorldUITests/HellowWorldUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HellowWorldUITestsLaunchTests.swift
+//  HellowWorldUITests
+//
+//  Created by KYUCHEOL KIM on 2023/05/23.
+//
+
+import XCTest
+
+final class HellowWorldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
